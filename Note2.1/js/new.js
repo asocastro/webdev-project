b489ce84b87427f4cae3f4c3f94c23f8e3d6a609
BERT00001 <111269229+BERT00001@users.noreply.github.com>
2022-10-20 17:56:54 +0800

    Add files via upload

---
+++ Note2.1/js/new.js
@@ -0,0 +1,15 @@
+var UE_Email = document.getElementById('UE').value;
+var valid = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+function Validation_UEemail() {
+
+
+    if (UE_Email.match(valid)) {
+
+        alert('CORRECT UE EMAIL');
+    } else {
+
+        alert(ERROR);
+    }
+
+}
