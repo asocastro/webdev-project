31f1aa45fd65dfbd6e55b5394b3d31b30d48e4eb
asocastro <77965653+asocastro@users.noreply.github.com>
2022-10-25 21:24:48 +0800

    Updated mp3 js
    finished personal info

--- M-MP3 Form Validation/js/main.js
+++ M-MP3 Form Validation/js/main.js
@@ -1,58 +1,3 @@
-
-
-$(function () {
-    $("#wizard").steps({
-        headerTag: "h2",
-        bodyTag: "section",
-        transitionEffect: "fade",
-        enableAllSteps: true,
-        transitionEffectSpeed: 500,
-        labels: {
-            finish: "Submit",
-            next: "Next",
-            previous: "Previous"
-        },
-        finish: function(){
-            alert("test")
-        
-        }
-        
-    });
-    
-    $('.wizard > .steps li a').click(function () {
-        $(this).parent().addClass('checked');
-        $(this).parent().prevAll().addClass('checked');
-        $(this).parent().nextAll().removeClass('checked');
-    });
-    $('.Next').click(function () {
-        $("#wizard").steps('next');
-    })
-    $('.Previous').click(function () {
-        $("#wizard").steps('previous');
-    })
-    $('.Submit').click(function () {
-        $("#wizard").steps('finish');
-    })
-    $('html').click(function () {
-        $('.select .dropdown').hide();
-    });
-    $('.select').click(function (event) {
-        event.stopPropagation();
-    });
-    $('.select .select-control').click(function () {
-        $(this).parent().next().toggle();
-    })
-    $('.select .dropdown li').click(function () {
-        $(this).parent().toggle(); var text = $(this).attr('rel'); $(this).parent().prev().find('div').text(text);
-    })
-    $('.Submit').click(function(){ 
-        window.alert("test"); 
-        return false; 
-    });
-})
-
-
-
 //Personal info
 var surname = document.getElementById("Surname").value;
 var given_name = document.getElementById("Given_Name").value;
@@ -85,4 +30,115 @@ var membership_type = document.getElementById("Membership_type").value;
 var proof = document.getElementById("proof").value;
 var accept = document.getElementById("accept").value;
 
+
+$(function () {
+    $("#wizard").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        enableContentCache: true,
+        transitionEffectSpeed: 500,
+
+        onFinishing: function (event, currentIndex) {
+
+            for (var i = 0; i < document.myForm.Year.length; i++) {
+                if (document.myForm.Year[i].checked) {
+                    var rad_val = document.myForm.Year[i].value;
+                }
+            }
+
+            //personal info
+            /*if(document.getElementById("Surname").value == "" ) {
+                window.alert("Please provide your surname.");
+                return false;
+            }else if(document.getElementById("Given_Name").value == "" ) {
+                window.alert("Please provide your given name.");
+                return false;
+            }else if(document.getElementById("Std_Num").value.length!=11 ) {
+                window.alert("Please enter a valid student number.");
+                console.log(document.getElementById("Std_Num").value.length);
+                return false;
+            }else if (document.getElementById("yr1").checked == false && 
+            document.getElementById("yr2").checked == false &&
+            document.getElementById("yr3").checked == false && 
+            document.getElementById("yr4").checked == false) {
+                window.alert("Please choose your year level.");
+                return false;
+            }else*/ if (document.getElementsByName("program").selected == false) {
+                window.alert("Please choose a program.");
+                console.log(document.getElementById("program").value);
+                return false;
+            } else {
+                return true;
+            }
+        },
+        onFinished: function (event, currentIndex) {
+            window.alert("Succesfully registered!");
+        },
+        labels: {
+            finish: "Submit",
+            next: "Next",
+            previous: "Previous"
+        },
+    });
+
+    $('.wizard > .steps li a').click(function () {
+        $(this).parent().addClass('checked');
+        $(this).parent().prevAll().addClass('checked');
+        $(this).parent().nextAll().removeClass('checked');
+    });
+    $('.Next').click(function () {
+        $("#wizard").steps('next');
+    })
+    $('.Previous').click(function () {
+        $("#wizard").steps('previous');
+    })
+    $('.Submit').click(function () {
+        $("#wizard").steps('finish');
+    })
+    $('html').click(function () {
+        $('.select .dropdown').hide();
+    });
+    $('.select').click(function (event) {
+        event.stopPropagation();
+    });
+    $('.select .select-control').click(function () {
+        $(this).parent().next().toggle();
+    })
+    $('.select .dropdown li').click(function () {
+        $(this).parent().toggle(); var text = $(this).attr('rel'); $(this).parent().prev().find('div').text(text);
+    })
+    $('.Submit').click(function () {
+        window.alert("test");
+        return false;
+    });
+
+})
+
+function validate() {
+    //Personal info
+    if (document.myForm.surname.value == "") {
+        window.alert("Please provide your name!");
+        document.myForm.surname.focus();
+        return false;
+    }/*
+    if( document.myForm.EMail.value == "" ) {
+       alert( "Please provide your Email!" );
+       document.myForm.EMail.focus() ;
+       return false;
+    }
+    if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
+       document.myForm.Zip.value.length != 5 ) {
+       
+       alert( "Please provide a zip in the format #####." );
+       document.myForm.Zip.focus() ;
+       return false;
+    }
+    if( document.myForm.Country.value == "-1" ) {
+       alert( "Please provide your country!" );
+       return false;
+    }
+    return( true );*/
+}
 
