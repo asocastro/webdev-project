30a84a2b1197c4bacd8b5dae08430b14823bc128
asocastro <77965653+asocastro@users.noreply.github.com>
2022-10-25 15:32:11 +0800

    Added code to main.js
    Added variables

--- M-MP3 Form Validation/js/main.js
+++ M-MP3 Form Validation/js/main.js
@@ -11,15 +11,49 @@ $(function () {
              previous: "Back" 
             } 
         }); 
-        $('.wizard > .steps li a').click(function () {
+    $('.wizard > .steps li a').click(function () {
              $(this).parent().addClass('checked'); 
              $(this).parent().prevAll().addClass('checked'); 
              $(this).parent().nextAll().removeClass('checked'); 
             }); 
-            $('.forward').click(function () { $("#wizard").steps('next'); 
+    $('.forward').click(function () {
+         $("#wizard").steps('next'); 
     })
     $('.backward').click(function () { $("#wizard").steps('previous'); })
     $('html').click(function () { $('.select .dropdown').hide(); }); $('.select').click(function (event) { event.stopPropagation(); }); $('.select .select-control').click(function () { $(this).parent().next().toggle(); })
     $('.select .dropdown li').click(function () { $(this).parent().toggle(); var text = $(this).attr('rel'); $(this).parent().prev().find('div').text(text); })
 
 })
+
+
+//Personal info
+var surname = document.getElementById("Surname").value;
+var given_name = document.getElementById("Given_Name").value;
+var middle_name = document.getElementById("Middle_Name").value;
+var stud_num = document.getElementById("Std_Num").value;
+var year_level = document.getElementById("Current_yr").value;
+var program = document.getElementById("program").value;
+
+//contact info
+var email = document.getElementById("email").value;
+var pemail = document.getElementById("pemail").value;
+var number = document.getElementById("number").value;
+var address = document.getElementById("address").value;
+var paddress = document.getElementById("paddress").value;
+
+//socmed
+var fb = document.getElementById("fb").value;
+var insta = document.getElementById("insta").value;
+var twitter = document.getElementById("twitter").value;
+
+//emergencies
+var ename = document.getElementById("FullName").value;
+var relationship = document.getElementById("relationship").value;
+var enumber = document.getElementById("enumber").value;
+var eaddress = document.getElementById("eaddress").value;
+var samehome = document.getElementById("samehome").value;
+
+//membership type
+var membership_type = document.getElementById("Membership_type").value;
+var proof = document.getElementById("proof").value;
+var accept = document.getElementById("accept").value;
